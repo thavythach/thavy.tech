49d5f438ede3baed315f29b5d6195a18fc78e3db
Thavy Thach <thavythach@gmail.com>
2020-12-08 19:29:49 -0600

    (#6) ng update @angular/cli --force

--- src/app/components/content-left-parallax-view/content-left-parallax-view.component.spec.ts
+++ src/app/components/content-left-parallax-view/content-left-parallax-view.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ContentLeftParallaxViewComponent } from './content-left-parallax-view.component';
 
@@ -6,7 +6,7 @@ describe('ContentLeftParallaxViewComponent', () => {
   let component: ContentLeftParallaxViewComponent;
   let fixture: ComponentFixture<ContentLeftParallaxViewComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ContentLeftParallaxViewComponent ]
     })
